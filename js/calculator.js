64caf6f2795b4a22989f176bf9a9dfab39e7a629
svdome <svdome@yandex.ru>
2022-02-08 16:24:19 +0300

    Создание главной ветки main

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+/* calculator.js */
+export function sum(a, b) {
+    return a + b
+    // your code
+  }
+  
+  export function subtract(a, b) {
+      return a - b
+    // your code
+  }
+  
+  export function multiply(a, b) {
+      return a * b
+    // your code
+  }
+  
+  export function divide(a, b) {
+      return a / b
+    // your code
+  }
